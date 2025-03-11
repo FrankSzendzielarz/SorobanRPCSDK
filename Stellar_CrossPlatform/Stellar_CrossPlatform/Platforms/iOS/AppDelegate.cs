d4a18b1e370e579bfc0b2538f881bde51b162231
Frank Szendzielarz <Frank@franks.technology>
2025-03-11 18:16:57 +0100

    Multiplatform boilerplate

---
+++ Stellar_CrossPlatform/Stellar_CrossPlatform/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Stellar_CrossPlatform;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
